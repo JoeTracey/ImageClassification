481bab770d6acafb05be5179bbd59365b110e782
Tracey <joseph.tracey@uconn.edu>
2021-03-27 03:28:14 -0400

    fixed batch size, added annotations

--- ResNet/test.py
+++ ResNet/test.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
 from torchsummary import summary
-import torchvision.models as models
 from random import randint
 from matplotlib import pyplot as plt
 
@@ -9,45 +8,54 @@ from model import ResNet50
 from load_MITDogs import load_dataset, create_breed_dataset
 from tqdm import tqdm
 
-lr = 1e-4
-steps_per_epoch =  50
-epochs = 20
-classes = 10
-batch_size = 2
+# Set Parameters for Session
+lr = 1e-5                   #learning rate - float effects training sensitivity
+steps_per_epoch =  50       #Number of times model sees data and is updated per epoch
+epochs = 600                #Number of epochs model will be trained for (epochs*steps_per_epoch), Loss results are averaged per epoch
+classes = 10                #Number of breeds being considered
+batch_size = 6              #Number of images seen by model per step
 
 
-# device='cpu'
+# Select device, either 'cpu' or 'cuda:(gpu-id)'
 device = 'cuda:0'
 
-session_name = "classes"+str(classes)+"_epoch"+str(epochs)+"_steps"+str(steps_per_epoch)+"_lr1e"+str(lr)[-1]+"_bs"+str(batch_size)
+#name for training session, records details
+session_name = "classes"+str(classes)+"_epoch"+str(epochs)+"_steps"+str(steps_per_epoch)+"_lr1e"+str(lr)+"_bs"+str(batch_size)
 print(session_name)
-#define training function
+
+
+#train model to classify images
 def train(model, epochs, trainx, trainy, steps_per_epoch , batch_size=2, results_dir="./results" ):
     max_sample = len(trainx)
     loss_log = []
+    #Model is trained for i-many epochs
     for i in tqdm(range(epochs)):
         epoch_loss = 0
+        #Model makes s-many training steps per epoch
         for s in range(steps_per_epoch):
-            sample = randint(0, max_sample-1)
             c, x, y = np.shape(train_x[0,0])
+            #Create new tensors to store
             x_in, y_true = torch.tensor(np.zeros((batch_size, c, x, y))), torch.Tensor(np.zeros((batch_size)))
             for bs in range(batch_size):
+                #pick a random training sample and put the correlated image and class_id into the tensors for usage
+                sample = randint(0, max_sample-1)
                 x_in[bs] = train_x[sample]
                 y_true[bs] = torch.Tensor([train_y[sample]])
+            #Set datatype and move tensors to GPU
             x_in , y_true = x_in.float().cuda(), y_true.long().cuda()
-            optimizer.zero_grad()
-
-            y_out = model(x_in)
-            loss = criterion(y_out, y_true)
-            loss.backward()
-            optimizer.step()
-            epoch_loss += loss.detach().cpu().numpy()
+            optimizer.zero_grad()                        #Reset gradient on optimizer
+            y_out = model(x_in)                          #Apply model to
+            loss = criterion(y_out, y_true)              #
+            loss.backward()                              #
+            optimizer.step()                             #
+            epoch_loss += loss.detach().cpu().numpy()    #
 
         epoch_loss = epoch_loss/steps_per_epoch
         loss_log+=[epoch_loss]
         print((i, epoch_loss))
     return(model,loss_log)
 
+# Predict model on data without training, returns
 def test_model(model, test_x, test_y):
     true_count = np.zeros((classes))
     total_count = np.zeros((classes))
@@ -59,10 +67,8 @@ def test_model(model, test_x, test_y):
         total_count[truth]+=1
     return(true_count, total_count)
 
-#load premade resnet50 as sample
 
 if __name__ == '__main__':
-    resnet50 = models.resnet50()
 
 
     # load dataset
@@ -71,7 +77,7 @@ if __name__ == '__main__':
     print('Making breed classification dataset.')
     train_x, train_y, test_x, test_y =create_breed_dataset(database, classes)
 
-    #load Resnet50 model from local folder
+    #load Resnet50 model from local file
     model = ResNet50(classes=classes)
 
 
@@ -79,22 +85,26 @@ if __name__ == '__main__':
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
+    #Convert model to cuda then train
     model = model.cuda()
     print('Starting training session. Please Wait.')
     model, log = train(model, epochs, train_x, train_y, steps_per_epoch, batch_size)
-    print('Training Complete')
-    plt.plot(range(epochs), log)
-    print(session_name)
+    print('Training Complete: '+session_name)
 
+    #log average loss vs epoch on training data
+    plt.plot(range(epochs), log)
+    plt.xlabel('Epoch')
+    plt.ylabel('CrossEntropy Loss')
     plt.savefig('results/'+session_name+'.png')
     plt.show()
 
+    #Apply model to test data, then print out accuracy by breed
     print("Testing model Performance")
     true_count, total_count=test_model(model, test_x, test_y)
     print('results')
     print(true_count)
     print(total_count)
-    accuracy = true_count/total_count
+    accuracy = true_count*100/total_count
     print(accuracy)
     breeds=[]
     for i in range(classes):
